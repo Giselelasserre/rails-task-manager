045967814225273dec62948cf041a2c7b4869180
Giselelasserre <giseleslasserre@gmail.com>
2017-08-08 17:07:08 -0300

    4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-
-  def index
-
-    @tasks = Task.all
-
-  end
-
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def new
-    ?  # We'll see that in a moment.
-  end
-
-  def create
-    @task = ?  # We'll see that in a moment.
-
-  end
-
-  def edit
-    @task = Task.find(params[:id])
-  end
-
-  def update
-    @task = Task.find(params[:id])
-    @task.? # We'll see that in a moment.
-  end
-
-   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "tasks", to: "tasks#all"
-  get "tasks/:id", to: "tasks#show"
+  get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
-  delete "tasks/:id", to: "tasks#destroy"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  post "tasks", to: "tasks#create", as: "create"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "tasks/:id", to: "tasks#update", as: "update"
+  delete "tasks/:id", to: "tasks#destroy", as: "destroy"
 end
 
 
+# tem que usar as task no show
+# o new tem q ser antes do show sempre para não dar problema
 # GET '/tasks': get all your tasks.
 # GET '/tasks/:id': get a precise task, e.g GET '/tasks/3' fetches the task with id=3
 # GET '/tasks/new': get the form to create a new task
