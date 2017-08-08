902c8d3330f88be96d56022bacf3def883f5c469
Giselelasserre <giseleslasserre@gmail.com>
2017-08-08 12:34:21 -0300

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "tasks", to: "tasks#all"
+  get "tasks/:id", to: "tasks#show"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
+
+
+# GET '/tasks': get all your tasks.
+# GET '/tasks/:id': get a precise task, e.g GET '/tasks/3' fetches the task with id=3
+# GET '/tasks/new': get the form to create a new task
+# POST '/tasks': post a new task
+# GET '/tasks/:id/edit': get the form to edit an existing task
+# PATCH '/tasks/:id': update an existing task
+# DELETE '/tasks/:id': delete an existing task
